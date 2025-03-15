db4e7d81dadee325aa8be68e2b727faa3ed52fe2
Blagden254 <sitatiblagden@gmail.com>
2025-03-15 10:57:20 +0300

    my simple calculator

--- calculator.py
+++ calculator.py
@@ -1,1 +1,24 @@
-git
+def simple_calculator(n,p, operation):
+#asking the user for a number and operation.
+ n=float(input("Enter the first number"))
+ p=float(input("enter the second number"))
+ operation=input("specify agin operation as +,*,-, or/")
+ if operation=="+":
+  result=n+p
+ elif operation=="-":
+  result=n-p
+ elif operation=="*": 
+  result=n*p
+ elif operation=="/":
+  if p==0:
+   print("Error:divion by zero is impossible")
+   return
+  result=n/p
+ else:
+  print("Error! select a valid operation")
+  return
+ print(f"{n} {operation} {p} = {result}")
+ #putting the calculatorn action by calling the function.
+simple_calculator() 
+ 
+ 
